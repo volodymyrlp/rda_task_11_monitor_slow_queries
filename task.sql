40cca9966ae8e5536398c1112f96684236fc70e8
volodymyrlp <mr.volodymyr@egmail.com>
2026-06-10 20:02:29 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,12 +1,12 @@
 create database IF NOT EXISTS ShopDB;
 use ShopDB;
--- Create tables
+
 CREATE TABLE Products1 (
     ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
--- Create test data in Products1 table
+
 INSERT INTO Products1 (Name) VALUES ('AwersomeProduct1');
 INSERT INTO Products1 (Name) VALUES ('AwersomeProduct2');
 INSERT INTO Products1 (Name) VALUES ('AwersomeProduct3');
